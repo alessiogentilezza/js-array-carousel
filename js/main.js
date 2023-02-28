0b74cb8f0c76dbd98af57793a887f73a4c3cf855
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-02-28 16:27:33 +0100

    aggiunto bottone

--- js/main.js
+++ js/main.js
@@ -6,12 +6,37 @@ const imgArray = [
     'img/05.webp'
 ];
 
-const containerImg = document.querySelector('.container-small');
+const containerImg = document.querySelector('.img-container');
+
+let containerImgContent = "";
 
 for (let i = 0; i < imgArray; i++) {
     const imgWrapper = `<div class="wrapper-img">
                                 <img src="${imgArray[i]}">
                             </div>`;
+    containerImgContent += imgWrapper;
 }
 
-console.log(imgArray)
+containerImg.innerHTML = containerImgContent;
+
+const wrapperImg = document.getElementsByClassName('wrapper-img');
+
+let activeImage = 0;
+
+wrapperImg[activeImage].classList.add('d-block');
+
+// console.log(wrapperImg);
+
+const nextDom = document.querySelector('#next');
+
+nextDom.addEventListener('click',
+    function () {
+        if (activeImage < wrapperImg.length - 1) {
+
+            wrapperImg[activeImage].classList.remove('d-block');
+            activeImage++;
+            wrapperImg[activeImage].classList.add('d-block');
+
+        }
+    }
+);
